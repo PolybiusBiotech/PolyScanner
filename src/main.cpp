2dc7a23f6481c4504ab2032adf0e79735ea077d1
Charles Yarnold <charlesyarnold@gmail.com>
2020-09-28 19:43:56 +0100

    Add first code to call to api based on coin presented

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,50 @@
 #include <TFT_eSPI.h>
 #include <Ticker.h>
 #include <driver/uart.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+
+
+WiFiMulti WiFiMulti;
+
+const char* root_ca PROGMEM = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIGEzCCA/ugAwIBAgIQfVtRJrR2uhHbdBYLvFMNpzANBgkqhkiG9w0BAQwFADCB\n" \
+"iDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0pl\n" \
+"cnNleSBDaXR5MR4wHAYDVQQKExVUaGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNV\n" \
+"BAMTJVVTRVJUcnVzdCBSU0EgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMTgx\n" \
+"MTAyMDAwMDAwWhcNMzAxMjMxMjM1OTU5WjCBjzELMAkGA1UEBhMCR0IxGzAZBgNV\n" \
+"BAgTEkdyZWF0ZXIgTWFuY2hlc3RlcjEQMA4GA1UEBxMHU2FsZm9yZDEYMBYGA1UE\n" \
+"ChMPU2VjdGlnbyBMaW1pdGVkMTcwNQYDVQQDEy5TZWN0aWdvIFJTQSBEb21haW4g\n" \
+"VmFsaWRhdGlvbiBTZWN1cmUgU2VydmVyIENBMIIBIjANBgkqhkiG9w0BAQEFAAOC\n" \
+"AQ8AMIIBCgKCAQEA1nMz1tc8INAA0hdFuNY+B6I/x0HuMjDJsGz99J/LEpgPLT+N\n" \
+"TQEMgg8Xf2Iu6bhIefsWg06t1zIlk7cHv7lQP6lMw0Aq6Tn/2YHKHxYyQdqAJrkj\n" \
+"eocgHuP/IJo8lURvh3UGkEC0MpMWCRAIIz7S3YcPb11RFGoKacVPAXJpz9OTTG0E\n" \
+"oKMbgn6xmrntxZ7FN3ifmgg0+1YuWMQJDgZkW7w33PGfKGioVrCSo1yfu4iYCBsk\n" \
+"Haswha6vsC6eep3BwEIc4gLw6uBK0u+QDrTBQBbwb4VCSmT3pDCg/r8uoydajotY\n" \
+"uK3DGReEY+1vVv2Dy2A0xHS+5p3b4eTlygxfFQIDAQABo4IBbjCCAWowHwYDVR0j\n" \
+"BBgwFoAUU3m/WqorSs9UgOHYm8Cd8rIDZsswHQYDVR0OBBYEFI2MXsRUrYrhd+mb\n" \
+"+ZsF4bgBjWHhMA4GA1UdDwEB/wQEAwIBhjASBgNVHRMBAf8ECDAGAQH/AgEAMB0G\n" \
+"A1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAbBgNVHSAEFDASMAYGBFUdIAAw\n" \
+"CAYGZ4EMAQIBMFAGA1UdHwRJMEcwRaBDoEGGP2h0dHA6Ly9jcmwudXNlcnRydXN0\n" \
+"LmNvbS9VU0VSVHJ1c3RSU0FDZXJ0aWZpY2F0aW9uQXV0aG9yaXR5LmNybDB2Bggr\n" \
+"BgEFBQcBAQRqMGgwPwYIKwYBBQUHMAKGM2h0dHA6Ly9jcnQudXNlcnRydXN0LmNv\n" \
+"bS9VU0VSVHJ1c3RSU0FBZGRUcnVzdENBLmNydDAlBggrBgEFBQcwAYYZaHR0cDov\n" \
+"L29jc3AudXNlcnRydXN0LmNvbTANBgkqhkiG9w0BAQwFAAOCAgEAMr9hvQ5Iw0/H\n" \
+"ukdN+Jx4GQHcEx2Ab/zDcLRSmjEzmldS+zGea6TvVKqJjUAXaPgREHzSyrHxVYbH\n" \
+"7rM2kYb2OVG/Rr8PoLq0935JxCo2F57kaDl6r5ROVm+yezu/Coa9zcV3HAO4OLGi\n" \
+"H19+24rcRki2aArPsrW04jTkZ6k4Zgle0rj8nSg6F0AnwnJOKf0hPHzPE/uWLMUx\n" \
+"RP0T7dWbqWlod3zu4f+k+TY4CFM5ooQ0nBnzvg6s1SQ36yOoeNDT5++SR2RiOSLv\n" \
+"xvcRviKFxmZEJCaOEDKNyJOuB56DPi/Z+fVGjmO+wea03KbNIaiGCpXZLoUmGv38\n" \
+"sbZXQm2V0TP2ORQGgkE49Y9Y3IBbpNV9lXj9p5v//cWoaasm56ekBYdbqbe4oyAL\n" \
+"l6lFhd2zi+WJN44pDfwGF/Y4QA5C5BIG+3vzxhFoYt/jmPQT2BVPi7Fp2RBgvGQq\n" \
+"6jG35LWjOhSbJuMLe/0CjraZwTiXWTb2qHSihrZe68Zk6s+go/lunrotEbaGmAhY\n" \
+"LcmsJWTyXnW0OMGuf1pGg+pRyrbxmRE1a6Vqe8YAsOf4vmSyrcjC8azjUeqkk+B5\n" \
+"yOGBQMkKW+ESPMFgKuOXwIlCypTPRpgSabuY0MLTDXJLR27lk8QyKGOHQ+SwMj4K\n" \
+"00u/I5sUKUErmgQfky3xxzlIPK1aEn8=\n" \
+"-----END CERTIFICATE-----\n";
 
 /**
  * 19 X SPEAKER_PWD
@@ -399,11 +443,42 @@ void setup() {
   tft.setCursor(0, 0);
 
   if (TFT_BL > 0) {
-    pinMode(TFT_BL, OUTPUT);
-    digitalWrite(TFT_BL, HIGH);
+    // configure LED PWM functionalitites
+    ledcSetup(5, 500, 8);
+    // attach the channel to the GPIO to be controlled
+    ledcAttachPin(TFT_BL, 5);
+    // Set brightness 0 - 255
+    ledcWrite(5, 25);
   }
 
-  spisd_test();
+  printIP5306Settings();
+
+  drawScreenLayout();
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString("BOOTING, PLEASE WAIT", tft.width() / 2, tft.height() / 2);
+
+  // SET UP WIFI AND STUFF FOR RESTING REST API
+  WiFiMulti.addAP("Bifrost", "lolbutts");
+  Serial.print("WiFi Connecting..."); 
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  drawScreenLayout();
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString("WiFi Connected, IP:", tft.width() / 2, tft.height() / 2);
+  tft.drawString(WiFi.localIP().toString(), tft.width() / 2, (tft.height() / 2)+10);
+
+  delay(3000);
+
+  //spisd_test();
   buzzer_test();
   button_init();
 
@@ -412,36 +487,138 @@ void setup() {
 
 void loop() {
   // **********************************************************************************************
-  switch (state) {
-  case 1:
+  if(state == 1){
     state = 0;
     // wifi_scan();
+    drawScreenLayout();
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.fillScreen(TFT_BLACK);
     tft.setTextDatum(MC_DATUM);
     tft.drawString("Read RFID", tft.width() / 2, tft.height() / 2);
-    scanForTag();
-    playSound();
-    break;
-  case 2:
+    printIP5306Stats();
+    activateBarcodeScan(1000);
+    // scanForTag();
+    // playSound();
+  }
+  else if(state == 2){
     state = 0;
+    drawScreenLayout();
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.fillScreen(TFT_BLACK);
     tft.setTextDatum(MC_DATUM);
     tft.drawString("Buzzer Test", tft.width() / 2, tft.height() / 2);
     playSound();
-    break;
-  case 3:
+  }
+  else if(state == 3){
     state = 0;
-    listDir(SD, "/", 2);
-    break;
-  case 4:
-    state = 0;
-    tft.fillScreen(TFT_BLACK);
+
+    drawScreenLayout();
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
     tft.setTextDatum(MC_DATUM);
-    tft.drawString("Undefined function", tft.width() / 2, tft.height() / 2);
-    break;
-  default:
-    break;
+    tft.drawString("Please present coin", tft.width() / 2, tft.height() / 2);
+
+    uint8_t success;
+    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+    uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+    
+    if (success) {
+      // Display some basic information about the card
+      Serial.println("Found an ISO14443A card");
+      Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+      Serial.print("  UID Value: ");
+      nfc.PrintHex(uid, uidLength);
+      Serial.println("");
+      
+      if (uidLength == 7)
+      {
+        drawScreenLayout();
+        tft.setTextColor(TFT_GREEN, TFT_BLACK);
+        tft.setTextDatum(MC_DATUM);
+        tft.drawString("Coin read, accessing data", tft.width() / 2, tft.height() / 2);
+
+        uint8_t uriIdentifier = 0;
+        char buffer[60];
+        memset(buffer, 0, 60);
+        if(nfc.ntag2xx_ReadNDEFString(&uriIdentifier, buffer, 60)){
+          WiFiClientSecure *client = new WiFiClientSecure;
+          if(client) {
+            client -> setCACert(root_ca);
+            {
+              // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is 
+              HTTPClient https;
+              Serial.print("[HTTPS] begin...\n");
+              if (https.begin(*client, String("https://") + String(buffer))) {  // HTTPS
+                Serial.print("[HTTPS] GET...\n");
+                // start connection and send HTTP header
+                int httpCode = https.GET();
+                // httpCode will be negative on error
+                if (httpCode > 0) {
+                  // HTTP header has been send and Server response header has been handled
+                  Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+                  // file found at server
+                  if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+                    String payload = https.getString();
+                    Serial.println(payload);
+                    drawScreenLayout();
+                    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+                    tft.setTextDatum(TL_DATUM);
+                    if(payload.length()<=40){
+                      tft.drawString(payload, 0, tft.height() / 2);
+                    }
+                    else{
+                      uint8_t line = 0;
+                      uint8_t stringlen = 0;
+                      for(uint8_t x = 0; x < payload.length(); x += 40){
+                        if(payload.length() - x < 40){
+                          stringlen = payload.length();
+                        }
+                        else{
+                          stringlen = 40 * (x + 1);
+                        }
+                        tft.drawString(payload.substring(40*line, stringlen), 0, (tft.height() / 2)+(8*line));
+                        line++;
+                      }
+                    }
+                  }
+                } else {
+                  Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+                  drawScreenLayout();
+                  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+                  tft.setTextDatum(MC_DATUM);
+                  tft.drawString("Network Error", tft.width() / 2, tft.height() / 2);
+                }
+                https.end();
+              } else {
+                Serial.printf("[HTTPS] Unable to connect\n");
+                drawScreenLayout();
+                tft.setTextColor(TFT_GREEN, TFT_BLACK);
+                tft.setTextDatum(MC_DATUM);
+                tft.drawString("Network Error", tft.width() / 2, tft.height() / 2);
+              }
+              // End extra scoping block
+            }
+            delete client;
+          } else {
+            Serial.println("Unable to create client");
+            drawScreenLayout();
+            tft.setTextColor(TFT_GREEN, TFT_BLACK);
+            tft.setTextDatum(MC_DATUM);
+            tft.drawString("Network Error", tft.width() / 2, tft.height() / 2);
+          }
+        }
+        else{
+          drawScreenLayout();
+          tft.setTextColor(TFT_GREEN, TFT_BLACK);
+          tft.setTextDatum(MC_DATUM);
+          tft.drawString("Coin had no address", tft.width() / 2, tft.height() / 2);
+        }
+      }
+    }
+    else{
+      drawScreenLayout();
+      tft.setTextColor(TFT_GREEN, TFT_BLACK);
+      tft.setTextDatum(MC_DATUM);
+      tft.drawString("Could not read Coin", tft.width() / 2, tft.height() / 2);
+    }
   }
 }
